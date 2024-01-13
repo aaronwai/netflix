75f205820c00cfda29a5a1f7e88f108bb94b145b
aaronwai <aaron.lung.wai@gmail.com>
2024-01-13 23:34:41 +0800

    Step 13 : index page 2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
 import { useState, useCallback } from "react";
 import MobileMenu from "./MobileMenu";
 import NavbarItem from "./NavbarItem";
-import { BsChevronDown } from "react-icons/bs";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
   }, []);
   return (
     <nav className='w-full fixed z-40'>
@@ -27,8 +34,34 @@ const Navbar = () => {
           className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'
         >
           <p className='text-white text-sm'>browse</p>
-          <BsChevronDown className=' text-white transition' />
+          <BsChevronDown
+            className={` text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
           <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className='flex flex-row ml-auto gap-7 items-center'>
+          <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+            <BsSearch />
+          </div>
+          <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+            <BsBell />
+          </div>
+          <div
+            onClick={toggleAccountMenu}
+            className='flex flex-row items-center gap-2 cursor-pointer relative'
+          >
+            <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
+              <img src='/images/default-blue.png' alt='' />
+            </div>
+            <BsChevronDown
+              className={` text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
         </div>
       </div>
     </nav>
