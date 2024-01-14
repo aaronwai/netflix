3599c5912233a8ef6f0bd9999bd19cf0eb97d831
aaronwai <aaron.lung.wai@gmail.com>
2024-01-14 10:20:41 +0800

    Step 16 : movie list layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
   if (!session) {
@@ -18,11 +19,15 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   );
 }
